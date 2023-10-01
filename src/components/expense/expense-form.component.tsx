5cb9d0b9a7db8d8087bb4922e1343aa50a138ef5
Trevor Chinn <revolution.x@gmail.com>
2023-10-02 01:33:21 +0900

    Added bootstrap and basic styling

--- src/components/expense/expense-form.component.tsx
+++ src/components/expense/expense-form.component.tsx
@@ -1,5 +1,7 @@
-import { request } from "http";
 import { useEffect, useState } from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
 // const defaultOptions = [
 //   { value: '', text: '--Choose an option--' },
@@ -41,7 +43,14 @@ const ExpenseForm = () => {
     });
   };
 
+  const handleChangeTextArea = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
 
+    setFormFields({
+      ...formFields,
+      [name]: value
+    });
+  };
 
   useEffect(() => {
     const getCategories = async () => {
@@ -63,10 +72,7 @@ const ExpenseForm = () => {
   }, [])
 
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
-    console.log("hey!")
     e.preventDefault();
-
-    console.log(formFields);
 
     const myBody = {
       ...formFields,
@@ -92,28 +98,45 @@ const ExpenseForm = () => {
   }
 
   return (
-    <div className="expense-form">
-      <form onSubmit={handleSubmit}>
-        <label>Category</label>
-        <select value={formFields.category} name="category" onChange={handleSelectChange}>
-          {categories.map(option => (
-            <option key={option.id} value={option.id}>{option.name}</option>
-          ))}
-        </select>
-        {
-          formFields.category === 'other' ? <input type="text" placeholder="Add new category" onChange={handleChange}></input> : ""
-        }
-        <label>Description</label>
-        <input type="text" name="description" minLength={2} required onChange={handleChange}></input>
-        <label>Date of Purchase</label>
-        <input type="date" name="expense_date" onChange={handleChange}></input>
-
-        <label>Cost</label>
-        <input type="number" name="cost" placeholder="Value in ¥" min="100" onChange={handleChange} />
-
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <Container>
+      <div className="col-lg-6 col-md-8 mx-auto">
+        <h1 className="h3 mb-3 fw-normal">Expense Entry</h1>
+        <form onSubmit={handleSubmit} className="text-start">
+          <Row>
+            <div className="col-sm-6 mb-3">
+              <label htmlFor="form_category" className="form-label">Category</label>
+              <select value={formFields.category} name="category" id="form_category" className="form-select" onChange={handleSelectChange} aria-label="Default select example">
+                {categories.map(option => (
+                  <option key={option.id} value={option.id} >{option.name}</option>
+                ))}
+              </select>
+              {
+                formFields.category === 'Other' ? <input type="text" placeholder="Add new category" name="new_category" onChange={handleChange}></input> : ""
+              }
+            </div>
+            <div className="col-sm-6 mb-3">
+              <label htmlFor="form_cost" className="form-label">Cost</label>
+              <div className="input-group">
+                <span className="input-group-text" id="basic-addon1">¥</span>
+                <input type="number" className="form-control" name="cost" id="form_cost" placeholder="" min="100" onChange={handleChange} required />
+              </div>
+              <div className="invalid-feedback">
+                Value must be greater than 100.
+              </div>
+            </div>
+          </Row>
+          <div className="col-12 mb-3">
+            <label htmlFor="form_description" className="form-label">Description</label>
+            <textarea className="form-control" name="description" id="form_description" minLength={2} required onChange={handleChangeTextArea} placeholder="Description of expense"></textarea>
+          </div>
+          <div className="col-12 mb-3">
+            <label htmlFor="form_expense_date" className="form-label">Date of Purchase</label>
+            <input type="date" className="form-control" id="form_expense_date" name="expense_date" onChange={handleChange} required></input>
+          </div>
+          <button type="submit" className="w-100 btn btn-lg btn-primary">Submit</button>
+        </form>
+      </div>
+    </Container>
   );
 };
 
--- src/routes/home/home.component.tsx
+++ src/routes/home/home.component.tsx
@@ -1,9 +1,8 @@
 import ExpenseForm from '../../components/expense/expense-form.component';
-import logo from '../../logo.svg';
 
 const Home = () => {
   return (
-    <div className="App">
+    <div className='px-4 pt-5 my-5 text-center'>
       <ExpenseForm></ExpenseForm>
     </div>
   );
