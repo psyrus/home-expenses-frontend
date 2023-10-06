4d0be68cb19d94a5bcb24afd6197c9ae5d66a381
Trevor Chinn <revolution.x@gmail.com>
2023-10-07 01:43:22 +0900

    remove unnecessary import

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,6 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
-import ApiClient from './utils/backend-api';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
