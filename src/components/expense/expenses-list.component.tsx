0027044d1ce4f9b0506a6a6bce9fe703f75f9d2c
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-10-08 16:18:07 +0800

    oneline

--- src/components/expense/expenses-list.component.tsx
+++ src/components/expense/expenses-list.component.tsx
@@ -43,12 +43,8 @@ const ExpensesList = () => {
             direction = 'desc';
         }
         const sortedData = [...expensesData].sort((a, b) => {
-            if (a[key] < b[key]) {
-                return direction === 'asc' ? -1 : 1;
-            }
-            if (a[key] > b[key]) {
-                return direction === 'asc' ? 1 : -1;
-            }
+            if (a[key] < b[key]) return direction === 'asc' ? -1 : 1;
+            if (a[key] > b[key]) return direction === 'asc' ? 1 : -1;
             return 0;
         })
         console.log(sortedData)
