5a0087c75a73aec539e7ffbf5b26616fbe423938
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-10-08 15:59:50 +0800

    This isn't a terrible commit message
    oneline

--- src/components/expense/expenses-list.component.tsx
+++ src/components/expense/expenses-list.component.tsx
@@ -34,6 +34,23 @@ export type UserApiResponse = {
 
 const ExpensesList = () => {
     const [expensesData, setExpensesData] = useState<ExpenseApiResponse[]>([]);
+    const [sortConfig, setSortConfig] = useState({key: '', direction: 'asc'})
+
+    const handleSort = (key: keyof ExpenseApiResponse) => {
+        console.log("sorting by", key)
+        let direction = 'asc';
+        if (sortConfig.key === key && sortConfig.direction === 'asc') {
+            direction = 'desc';
+        }
+        const sortedData = [...expensesData].sort((a, b) => {
+            if (a[key] < b[key]) return direction === 'asc' ? -1 : 1;
+            if (a[key] > b[key]) return direction === 'asc' ? 1 : -1;
+            return 0;
+        })
+        console.log(sortedData)
+        setSortConfig({ key, direction });
+        setExpensesData(sortedData)
+    }
 
     useEffect(() => {
         /**
@@ -74,13 +91,13 @@ const ExpensesList = () => {
                 <table className="table table-sm table-hover">
                     <thead>
                         <tr>
-                            <th scope="col">#</th>
-                            <th scope="col">Category</th>
-                            <th scope="col">Cost</th>
-                            <th scope="col">Description</th>
-                            <th scope="col">Registered by User</th>
-                            <th scope="col">Expense Date</th>
-                            <th scope="col">Paid Back</th>
+                            <th scope="col" onClick={() => handleSort('id')}>#</th>
+                            <th scope="col" onClick={() => handleSort('category')}>Category</th>
+                            <th scope="col" onClick={() => handleSort('cost')}>Cost</th>
+                            <th scope="col" onClick={() => handleSort('description')}>Description</th>
+                            <th scope="col" onClick={() => handleSort('registered_by_user')}>Registered by User</th>
+                            <th scope="col" onClick={() => handleSort('expense_date')}>Expense Date</th>
+                            <th scope="col" onClick={() => handleSort('paid_back')}>Paid Back</th>
                         </tr>
                     </thead>
                     <tbody className="table-group-divider">
