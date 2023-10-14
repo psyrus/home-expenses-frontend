92fcc446e89f56c28293c18a919b7070d09b512e
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-10-15 01:16:50 +0800

    ???

--- src/components/expense/sortable-table2.component.tsx
+++ src/components/expense/sortable-table2.component.tsx
@@ -1,55 +1,62 @@
 import { useState, useMemo } from 'react'
 import { MiscItems } from '../../routes/misc/misc.component'
+import { Container } from "react-bootstrap";
 
 export type SortConfig = {
-  key: number
+  key: keyof MiscItems
   direction: string
 }
 
 const SortableTableTwo = (props: { items: MiscItems[] }) => {
-  console.log(props)
+  // console.log(props)
   const items = props.items
 
   const [sortedField, setSortedField] = useState<keyof MiscItems>('id')
   const [sortConfig, setSortConfig] = useState<SortConfig>({
-    key: 1,
+    key: 'id',
     direction: 'asc'
   })
 
   useMemo(() => {
-    let sortedItems = items
+    let dir = 'asc'
+    const sortedItems = items
 
+    if (sortConfig.key === sortedField && sortConfig.direction === 'asc') {
+      dir = 'desc'
+    }
     sortedItems.sort((a, b) => {
       // if (a[sortConfig.key] < b[sortConfig.key]) {
+      // if (a['name'] < b[sortConfig.key]) {
       if (a[sortedField] < b[sortedField]) {
+        console.log(...sortedItems)
         return sortConfig.direction === 'asc' ? -1 : 1
       }
       if (a[sortedField] > b[sortedField]) {
+        console.log(...sortedItems)
         return sortConfig.direction === 'asc' ? 1 : -1
       }
       return 0
     })
-
-    console.log(sortedItems)
-  }, [items, sortConfig])
+    setSortConfig({ key:sortedField, direction:dir })
+  }, [items, sortedField])
 
   return (
-    <table>
+    <table className="table table-sm table-hover">
       <caption>Our products</caption>
       <thead>
         <tr>
-          <th>Name</th>
-          <th>Price</th>
-          <th>In Stock</th>
+          <th onClick={() => setSortedField('name')}>Name</th>
+          <th onClick={() => setSortedField('price')}>Price</th>
+          <th onClick={() => setSortedField('stock')}>In Stock</th>
         </tr>
       </thead>
       <tbody>
         {/* {items.map((item: any) => ( */}
         {items.map((item: MiscItems) => (
           <tr key={item.id}>
-            <td onClick={() => setSortedField('name')}>{item.name}</td>
-            <td onClick={() => setSortedField('price')}>{item.price}</td>
-            <td onClick={() => setSortedField('stock')}>{item.stock}</td>
+            <td>{item.name}</td>
+            <td>{item.price}</td>
+            <td>{item.stock}</td>
           </tr>
         ))}
       </tbody>
