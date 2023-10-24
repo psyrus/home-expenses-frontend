b13a7ff561461b762a00eab8f98e84c53fc13395
Jerrif <6222577+Jerrif@users.noreply.github.com>
2023-10-24 22:45:09 +0800

    Added ability to sort the table yes

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './routes/home/home.component';
 import Navigation from './routes/navigation/navigation.component';
 import Expenses from './routes/expenses/expenses.component';
-import SortableTableTwo from './components/expense/sortable-table2.component';
 import './App.css';
 import AuthenticationHandler from './routes/authentication/authentication.component';
 
